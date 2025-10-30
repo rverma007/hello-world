a8306716da1363371ce88fdb52bb2fa7fd614017
Ruchika Verma <rverma@ex2india.local>
2025-10-30 11:28:35 +0530

    adding test file

---
+++ test_app.py
@@ -0,0 +1,10 @@
+
+import unittest
+from app import hello_world
+
+class TestApp(unittest.Test.case):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "Hello, World!")
+
+if __name__ =='__main__' :
+    unittest.main()      
